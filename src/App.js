9f4d3b25d79e430bd37db757ac2c1f49916a2d7a
jun-nguyen-go <jun.nguyen.goldenowl@gmail.com>
2022-03-02 15:31:21 +0700

    feat: add some changes 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React native
+          Learn React JS
         </a>
       </header>
     </div>
